6cc30880c1ec0f17f14bfaa4d2f60f1cb0921209
manqr <manqrdeveloper@gmail.com>
2019-10-03 23:31:41 +0700

    fix cashier payment

--- backend/controllers/KasirController.php
+++ backend/controllers/KasirController.php
@@ -62,9 +62,62 @@ class KasirController extends \yii\web\Controller
 
     public function actionCheckout(){
         
-        
-        
+        if($_POST){	        
+            	        
+            $cart = Cart::findOne($_POST['urutan']);	
+            $cart->jumlah_bayar = $_POST['bayar'];	
+            $cart->diskon = $_POST['diskon'];	
+            $cart->keterangan2 = $_POST['keterangan'];	
+            $cart->flag= 2;	
+            $cart->save();	
+            $model = new Kuitansi();	
+            $model->no_kuitansi = $_POST['kode'];	
+            $model->bank_name = $_POST['bank'];	
+            $model->keterangan2 = $_POST['keterangan'];	
+            $model->idcart = $cart->urutan;	
+            $model->kode_siswa = $cart->kode_siswa;	
+            $model->idtagihan = $cart->idtagihan;	
+            $model->remarks = $cart->remarks;	
+            $model->diskon = $_POST['diskon'];	
+            $model->keterangan = $cart->keterangan;	
+            $model->nominal = $cart->nominal;	
+            $model->jumlah_bayar = $_POST['bayar'];	
+            $model->tahun_ajaran = $cart->tahun_ajaran;	
+            $model->payment_method = $_POST['payment'];	
+            $model->flag = $cart->flag;	
+            $model->date = ($_POST['tgl'] != '' ? $_POST['tgl'] : date('Y-m-d H:i:s'));	
+            $model->save(false);	
+            if(strtolower($cart->keterangan) == 'spp'){	
+                \Yii::$app->db->createCommand("UPDATE tagihan_siswa_spp	
+                                                SET nominal = (nominal - ".$_POST['bayar']."),	
+                                                    date_update = '".date('Y-m-d H:i:s')."',	
+                                                    user_update = '".Yii::$app->user->identity->username ."',	
+                                                    flag = 2	
+                                                WHERE idtagihan = '".$model->idtagihan."'	
+                                                AND kode_siswa = '".$model->kode_siswa."'	
+                                                AND bulan = '".$cart->remarks."'")->execute();     	
+            }else if(strtolower($cart->keterangan) == 'tagihan') {	
+                 \Yii::$app->db->createCommand("UPDATE tagihan_siswa	
+                                                SET ".str_replace(' ','_',strtolower($cart->remarks))." = (".str_replace(' ','_',strtolower($cart->remarks))." - ".$_POST['bayar'].")	
+                                                WHERE idtagihan = '".$model->idtagihan."'	
+                                                AND kode_siswa = '".$model->kode_siswa."'")->execute();          	
+            }	
+            	
+            $data = [	
+                'msg'=>'success',	
+                'siswa'=>$model->kode_siswa,	
+                'no_kuitansi'=>$model->no_kuitansi	
+            ];	
+        }else{	
+            $data = [	
+                'msg'=>'Method not allowed'	
+            ];	
+           	
+        }	
+        Yii::$app->response->format = Response::FORMAT_JSON;	
+        return $data;	
     }
+        
 
     public function actionPrint($no_kuitansi){
         include 'inc/pdf.php';
